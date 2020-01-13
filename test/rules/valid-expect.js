0ab9fac989ba56d5726495f2a961d8f087882149
Balakumaran K <CR171018@gosi.ins>
2020-01-13 18:09:00 +0300

    fix(valid-expert): added condition to verify the expect and to be not have the same arguments

--- test/rules/valid-expect.js
+++ test/rules/valid-expect.js
@@ -10,11 +10,26 @@ eslintTester.run('valid-expect', rule, {
   valid: [
     'expect("something").toEqual("else");',
     'expect(true).toBeDefined();',
-    'expect([1, 2, 3]).toEqual([1, 2, 3]);',
     'expect(undefined).not.toBeDefined();'
   ],
 
   invalid: [
+    {
+      code: 'expect([1, 2, 3]).toBe([1, 2, 3]);',
+      errors: [
+        {
+          message: 'Both expect and verification argument should not be same'
+        }
+      ]
+    },
+    {
+      code: 'expect(true).toBe(true);',
+      errors: [
+        {
+          message: 'Both expect and verification argument should not be same'
+        }
+      ]
+    },
     {
       code: 'expect().toBe(true);',
       errors: [
